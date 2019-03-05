2d2f765975387ab869329112f0d328178399baec
Hassam Ali <hassam-ali1@outlook.com>
2019-03-05 22:02:58 +0500

    Views Setup rendering to template now

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from .models import Pet
 
 # Create your views here.
+def home(request):
+    # return HttpResponse('Home View Page')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
+
+def pet_detail(request, id):
+        #return HttpResponse('pet detail page and id is {}'.format(id))
+
+    try:
+        pet = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request, 'pet_detail.html', {'pet': pet})
